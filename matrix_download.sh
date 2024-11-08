199375ba10c740ed3ff2c3b3777abe9f9f969720
shelleydan <shelleydr@cardiff.ac.uk>
2024-11-08 11:56:36 +0000

    Added Extraction Method

--- matrix_download.sh
+++ matrix_download.sh
@@ -8,7 +8,7 @@
 
 #Setting Variables
 WORKDIR=$(pwd)
-GEOID=GSE217510
+GEOID=GSE217504
 GEOLINK=${GEOID::-3}nnn
 
 # sed -i 's/$GEOLINK/$GEOLINK\nnn/'
@@ -17,12 +17,33 @@ echo ${GEOID}
 echo ${GEOLINK}
 
 
-#######################Downloading Matrix#########################
+####################### Downloading Matrix #######################
 
-wget -O ${WORKDIR}/${GEOID}.matrix.txt.gz \
-	https://ftp.ncbi.nlm.nih.gov/geo/series/${GEOLINK}/${GEOID}/matrix/${GEOID}_series_matrix.txt.gz
+#wget -O ${WORKDIR}/${GEOID}.matrix.txt.gz \
+#	https://ftp.ncbi.nlm.nih.gov/geo/series/${GEOLINK}/${GEOID}/matrix/${GEOID}_series_matrix.txt.gz
 
-pid=$!
-wait $pid
+#Assigned a jobid to the wget command to let the download finish before moving on
+#pid=$!
+#wait $pid
 
-gunzip ${GEOID}.matrix.txt.gz
+#The downloaded file is zipped and is useless without unzipping first.
+#gunzip ${GEOID}.matrix.txt.gz 
+
+#The above is all commented out to continue with the next part REMOVE THIS
+
+####################### Extracting Info #######################
+
+### Key Info to Extract ###
+# - !Sample_description - sample name (e.g. SKA for this assignment) 
+# - !Sample_source_name_ch1 - The Cell Line Used
+# - !Sample_characteristics_ch1 - infected vs mock
+# - !Sample_characteristics_ch1 - sample time
+
+cat ${GEOID}.matrix.txt | grep \
+				-e '!Sample_description'\
+				-e '!Sample_source_name_ch1'\
+				-e '!Sample_characteristics_ch1'\
+				-e '!Sample_characteristics_ch1'\
+				>> extracted_metadata.txt
+
+
