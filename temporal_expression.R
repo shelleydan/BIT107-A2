969c7fcfe3c7d6b9b518a76ae1b775b5110817e2
shelleydan <shelleydr@cardiff.ac.uk>
2024-11-12 14:39:48 +0000

    Initial Time-Course RNA-seq

---
+++ temporal_expression.R
@@ -0,0 +1,8 @@
+
+####################################################
+############ TEMPORTAL RNA-seq ANALYSIS ############
+####################################################
+
+## Reference ##
+#Varoquaux, N. and Purdom, E. 2020. A pipeline to analyse time-course gene expression data. 
+#Available at: https://f1000research.com/articles/9-1447 [Accessed: 12 November 2024].
