5fe0708b4cbb416573dd9eeaf5d8b377fd7cdde9
shelleydan <shelleydr@cardiff.ac.uk>
2024-11-13 14:25:55 +0000

    Model Works - Next Steps

--- temporal_expression.R
+++ temporal_expression.R
@@ -39,7 +39,6 @@ library(ggfortify)
 ################################################################################
 ############################# DATA INPUT & CLEANING ############################
 ################################################################################
-
 #NOTE!: Start with a Fresh Environment
 
 data <- read.csv("rawdata/GSE217504_host_counts_matrix.csv", #Input Counts Data
@@ -56,25 +55,27 @@ meta$Test <- as.factor(meta$Test)
 vals <- c(4, 12, 48) # Taking only the timepoints that have controls and tests
 meta <- meta[meta$Condition %in% vals, ] #Removing Bad Data from Targets
 meta <- data.frame(meta, row.names = 3) #Setting sample names as the rownames
+meta$Test[meta$Test == 'infected'] <- 'I'
+meta$Test[meta$Test == 'mock'] <- 'M'
 meta$Replicate <- c(1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3) #Manually adding replicate numbers
+str(meta)
+
 data <- data[, colnames(data) %in% row.names(meta)] #Removing bad data from Counts
 
-#DESeq2 doesn't like column names not in the same order as the targets, here I reorder the column names alpha-numerically
+#Moanin Model doesn't like column names not in the same order as the targets, here I reorder the column names alpha-numerically
 data_order = sort(colnames(data)) #Re-order column names to alpha-numerical
 data <- data[, data_order] #Apply the reordering
 
 #Checking Data
 summary(data)
-summary(testData)
-summary(counts_data)
 
 ################################################################################
 ################ COLOUR CODING FOR WHOLE SCRIPT STANDARDISATION ################
 ################################################################################
 
 #Defining Colours
-test_colours <- c("mock" = "Blue",
-                  "infected" = "Red")
+test_colours <- c("M" = "Blue",
+                  "I" = "Red")
 
 condition_colours <- c("4" = "Blue",
                        "12" = "Purple",
@@ -93,20 +94,21 @@ ann_markers <- list(
   Replicate = rep_marker
 )
 
+meta$Test <- factor(meta$Test, levels(meta$Test)[c(2,1)])
+#2 is Mock, 1 is infected
+
 ################################################################################
 ############################## TEMPORAL MODELLING ##############################
 ################################################################################
-
 moaninModel <- create_moanin_model(data=data, 
                                     meta=meta,
                                     group_variable = "Test",
                                     time_variable = "Condition")
 
 show(moaninModel) # Gives a summary of the produced model
-dim(moaninModel) #Dimensions of the model... its big
 
 #Defining groups to contrasts for analysis
-contrasts <- create_timepoints_contrasts(moaninModel, "mock", "infected") 
+contrasts <- create_timepoints_contrasts(moaninModel, "M", "I") 
 #These contrasts output all the groups for comparison for all the timepoints, e.g. mock.4hrs_vs_infected.4hrs
 
 hour_de_analysis <- DE_timepoints(moaninModel, contrasts, use_voom_weights = T) #Set vooms to F for microarray data
@@ -130,7 +132,10 @@ tabCombos<-table(signifCombos)
 #PLOT!: Gene Expression of top10 overtime. 
 head(hour_de_analysis)
 
-exampleGenes<-names(signifCombos[signifCombos=="4,12,48"][1:10])
+#top10DEGs are the top 10 DEGs in the 4hr analysis determined on DEG_Analysis.R
+
+exampleGenes<-names(signifCombos[signifCombos=="4,12,48"][row.names(top10DEGs)])
+exampleGenes <- row.names(top10DEGs)
 plot_splines_data(moaninModel, 
                   subset_data=exampleGenes,
                   colors=ann_colours$Test,
@@ -144,12 +149,25 @@ head(hour_de_analysis)
 #only contains infected or mock, this isn't possible.                          #
 ################################################################################
 
+
 ################################################################################
-############################# FOLD CHANGE ANALYSIS #############################
+########################### KEGG ANALYSIS OVER-TIME ############################
 ################################################################################
 
+#1. Filtering genes
+# Then rank by fisher's p-value and take max the number of genes of interest
+# Filter out q-values for the pvalues table
+fishers_pval = pvalues_fisher_method(pvalues)
+qvalues = apply(pvalues, 2, p.adjust)
+fishers_qval = p.adjust(fishers_pval)
 
-
+genes_to_keep = row.names(
+  log_fold_change_max[
+    (rowSums(log_fold_change_max > 2) > 0) &
+      (fishers_qval < 0.05), ])
+# Keep the data corresponding to the genes of interest in another variable.
+# by subsetting the `moanin_model`, which contains the data.
+de_moanin_model = moanin_model[genes_to_keep,]
 
 
 
