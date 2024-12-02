7d453db37b17b6eefa76782f82edd46bc5f8a987
shelleydan <shelleydr@cardiff.ac.uk>
2024-12-02 12:00:44 +0000

    Following New Analysis

--- 1_analysis_scripts/6_Profile_Analysis.R
+++ 1_analysis_scripts/6_Profile_Analysis.R
@@ -5,19 +5,103 @@
 
 # https://github.com/ACSoupir/Bioinformatics_YouTube.git
 
+
+## Library --------------------------------------------------------------------
+
+library(tidyverse)
+library(RColorBrewer)
+library(clusterProfiler)
+library(org.Hs.eg.db)
+library(DOSE)
+library(enrichplot)
+library(ggupset)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#OLD METHOD
+
+
 ## Library Prep ----------------------------------------------------------------
 BiocManager::install("pathview")
 BiocManager::install("gage")
 BiocManager::install("gageData")
 BiocManager::install("org.Hs.eg.db")
 BiocManager::install("clusterProfiler")
-BiocManager::install("biomartr")
+BiocManager::install("GOplot")
 library(pathview)
 library(gage)
 library(gageData)
-library(org.Hs.eg.db)
-library(AnnotationDbi)
+
+
 library(biomartr)
+
+library(AnnotationDbi)
+library(clusterProfiler)
+library(org.Hs.eg.db)
+library(GOplot)
 
 ## Importing GO Database -------------------------------------------------------
 
@@ -30,35 +114,87 @@ DEG4 <- read.csv("3_output_data/raw_DEG_results_4.csv",
                  header = T, row.names = 1)
 
 DEG12 <- read.csv("3_output_data/raw_DEG_results_12.csv", 
-                 header = T, row.names = 1)
+                  header = T, row.names = 1)
 
 DEG48 <- read.csv("3_output_data/raw_DEG_results_48.csv", 
-                 header = T, row.names = 1)
+                  header = T, row.names = 1)
 
 ## Filtering Data for Only Significant -----------------------------------------
 
-DEG4_Sig <- DEG4[DEG4$diffexpressed == 'UP' | DEG4$diffexpressed == 'DOWN',]
-DEG12_Sig <- DEG12[DEG12$diffexpressed == 'UP' | DEG12$diffexpressed == 'DOWN',]
-DEG48_Sig <- DEG48[DEG48$diffexpressed == 'UP' | DEG48$diffexpressed == 'DOWN',]
+DEG4_Sig_up <- rownames(DEG4[DEG4$diffexpressed == 'UP',])
+DEG4_Sig_down <- rownames(DEG4[DEG4$diffexpressed == 'DOWN',])
+DEG12_Sig_up <- rownames(DEG12[DEG12$diffexpressed == 'UP',])
+DEG12_Sig_down <- rownames(DEG12[DEG12$diffexpressed == 'DOWN',])
+DEG48_Sig <- DEG48[DEG48$diffexpressed == 'UP'| DEG48$diffexpressed == 'DOWN',]
+DEG48_Sig_d<- DEG48[DEG48$diffexpressed == 'DOWN',]
 
 rm(DEG4, DEG12, DEG48) # Remove un-filtered data frame
 
-## Assigning ENSAMBL IDs -------------------------------------------------------
+## GO Analysis 4HRS ------------------------------------------------------------
 
-symbols4 <- AnnotationDbi::select(org.Hs.eg.db,
-                                  keys = row.names(DEG4_Sig),
-                                  columns = c('ENTREZID'), 
-                                  keytype = 'SYMBOL')
+GO_4UP_res <- enrichGO(gene = DEG4_Sig_up, 
+                       OrgDb = "org.Hs.eg.db",
+                       keyType = 'SYMBOL',
+                       ont = "BP")
+GO4UP <- plot(barplot(GO_4UP_res, showCategory = 20))
 
-symbols12 <- AnnotationDbi::select(org.Hs.eg.db,
-                                  keys = row.names(DEG12_Sig),
-                                  columns = c('ENTREZID'), 
-                                  keytype = 'SYMBOL')
+GO_4DOWN_res <- enrichGO(gene = DEG4_Sig_down, 
+                         OrgDb = "org.Hs.eg.db",
+                         keyType = 'SYMBOL',
+                         ont = "BP")
+GO4DOWN <- plot(barplot(GO_4DOWN_res, showCategory = 20))
 
-symbols48 <- AnnotationDbi::select(org.Hs.eg.db,
-                                  keys = row.names(DEG48_Sig),
-                                  columns = c('ENTREZID'), 
-                                  keytype = 'SYMBOL')
+## GO Analysis 12HRS -----------------------------------------------------------
+
+GO_12UP_res <- enrichGO(gene = DEG12_Sig_up, 
+                        OrgDb = "org.Hs.eg.db",
+                        keyType = 'SYMBOL',
+                        ont = "BP")
+GO12UP <- plot(barplot(GO_12UP_res, showCategory = 20))
+
+GO_12DOWN_res <- enrichGO(gene = DEG12_Sig_down, 
+                          OrgDb = "org.Hs.eg.db",
+                          keyType = 'SYMBOL',
+                          ont = "BP")
+GO12DOWN <- plot(barplot(GO_12DOWN_res, showCategory = 20))
+
+## GO Analysis 48HRS -----------------------------------------------------------
+
+GO_48UP_res <- enrichGO(gene = DEG48_Sig_up, 
+                        OrgDb = "org.Hs.eg.db",
+                        keyType = 'SYMBOL',
+                        ont = "BP")
+GO48UP <- plot(barplot(GO_48UP_res, showCategory = 20))
+
+GO_48DOWN_res <- enrichGO(gene = DEG48_Sig_down, 
+                          OrgDb = "org.Hs.eg.db",
+                          keyType = 'SYMBOL',
+                          ont = "BP")
+head(GO48DOWN)
+
+GO48DOWN <- plot(barplot(GO_48DOWN_res, showCategory = 20))
+GO48DOWN
+
+data(EC)
+head(EC$david)
+
+testdata <- circle_dat(GO48DOWN, DEG48_Sig_d)
+
+list <- as.data.frame(rownames(DEG48_Sig))
+print(list)
+
+EC$genelist
+
+GOBubble(testdata, labels = 3)
+
+
+
+
+
+
+
+
+
 
 ## Producing an Array ----------------------------------------------------------
 foldChange_4 <- DEG4_Sig$log2FoldChange
@@ -93,14 +229,14 @@ view(gobpDEG4)
 
 #12HRS
 gobpDEG12 <- gage(exprs = foldChange_12,
-                 gsets =  gobpsets, 
-                 same.dir = T)
+                  gsets =  gobpsets, 
+                  same.dir = T)
 view(gobpDEG12)
 
 #48HRS
 gobpDEG48 <- gage(exprs = foldChange_48,
-                 gsets =  gobpsets, 
-                 same.dir = T)
+                  gsets =  gobpsets, 
+                  same.dir = T)
 view(gobpDEG4)
 
 ## KEGG Analysis ---------------------------------------------------------------
