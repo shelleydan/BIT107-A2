9cfef8f75f094ef3cf40b948d795d1ebdc8b8a88
shelleydan <shelleydr@cardiff.ac.uk>
2024-12-02 13:48:32 +0000

    Adding Pathway Enrichment Visualisations

--- 1_analysis_scripts/6_Profile_Analysis.R
+++ 1_analysis_scripts/6_Profile_Analysis.R
@@ -33,7 +33,7 @@ matrix_to_list <- function(pws){
 DEG4 <- read.csv("3_output_data/raw_DEG_results_4.csv", 
                  header = T)
 names(DEG4)[names(DEG4) == 'X'] <- 'gene_symbol'
-DEG4_Sig <- DEG4[DEG4$diffexpressed == 'UP'| DEG4$diffexpressed == 'DOWN',]
+DEG4_Sig <- DEG4[DEG4$diffexpressed == 'UP',] #| DEG4$diffexpressed == 'DOWN',]
 
 # Loading in our data (12HRS)
 DEG12 <- read.csv("3_output_data/raw_DEG_results_12.csv", 
@@ -68,24 +68,22 @@ gmt_files <- list.files(path = '5_Gene_Lists/',
 for (file in gmt_files) {
   pwl2 <- read.gmt(file)
   pwl2 <- pwl2[pwl2$gene %in% DEG_Genes,]
-  filename <- paste(gsub('c.\\.', '', gsub('.v2024.1.*$', '', file)), '.RDS', sep = '')
-  saveRDS(pwl2, filename)
+  filename_gs <- paste(gsub('c.\\.', '', gsub('.v2024.1.*$', '', file)), '.RDS', sep = '')
+  saveRDS(pwl2, filename_gs)
 }
 
-## Environment Housekeeping ---------------------------------------------------
-rm(DEG4)
-rm(DEG12)
-rm(DEG48)
+## Environment Housekeeping ----------------------------------------------------
 rm(DEG4_Sig)
 rm(DEG12_Sig)
 rm(DEG48_Sig)
 
-## Performing Cluster Analysis ------------------------------------------------
+## Filtering Gene Sets (REACTOME, KEGG, GO) ------------------------------------
 # We look for profiles enriched in both our up regulated and down regulated. 
 
 # Settings
 gene_path <- '5_Gene_Lists/'
 output <- '3_output_data/2_pathway_analysis/'
+
 name_of_comparison <- 'mockvsinfected' # for our filename
 background_genes <- 'reactome' # for our filename
 bg_genes <- readRDS(paste0(gene_path, 'reactome.RDS')) # background genes.
@@ -94,42 +92,70 @@ genecount_cutoff <- 5 # minimum number of genes in the pathway.
 filename <- paste0(output, 'clusterProfiler/', name_of_comparison, '_', background_genes) # filename of our PEA results
 
 if(background_genes == 'KEGG'){
-  bg_genes <- readRDS(paste0(bg_path, 'kegg.RDS'))
+  bg_genes <- readRDS(paste0(gene_path, 'kegg_legacy.RDS'))
 } else if(background_genes == 'reactome'){
-  bg_genes <- readRDS(paste0(bg_path, 'reactome.RDS'))
+  bg_genes <- readRDS(paste0(gene_path, 'reactome.RDS'))
 } else if(background_genes == 'go.bp'){
-  bg_genes <- readRDS(paste0(bg_path, 'go.bp.RDS'))
+  bg_genes <- readRDS(paste0(gene_path, 'go.bp.RDS'))
 } else {
   stop('Invalid background genes. Select one of the following: KEGG, Reactome, GO, or add new pwl to function')
 }
 
+## Performing Cluster Analysis -------------------------------------------------
+
+DEG4_res <- lapply(names(DEG4_res_list),
+                   function(x) enricher(gene = DEG4_res_list[[x]]$gene_symbol,
+                   TERM2GENE = bg_genes))
+
+names(DEG4_res) <- names(DEG4_res_list) # Apply the UP and DOWN tags
+
+DEG4_df_res <- lapply(names(DEG4_res), function(x) rbind(DEG4_res[[x]]@result))
+names(DEG4_df_res) <- names(DEG4_res)
+DEG4_df_res <- do.call(rbind,DEG4_df_res)
+
+# Subset the pathways by (i) padj value, (ii) Gene count
+
+target_pws <- unique(DEG4_df_res$ID[DEG4_df_res$p.adjust < padj_cutoff & DEG4_df_res$Count > genecount_cutoff])
+
+DEG4_df_res <- DEG4_df_res[DEG4_df_res$ID %in% target_pws,]
+
+## ENRICHMENT ------------------------------------------------------------------
+
+enrichres <- new("enrichResult",
+                 readable = FALSE,
+                 result = DEG4_df_res,
+                 pvalueCutoff = 0.05,
+                 pAdjustMethod = "BH",
+                 qvalueCutoff = 0.2,
+                 organism = "human",
+                 ontology = "UNKNOWN",
+                 gene = DEG4$gene_symbol,
+                 keytype = "UNKNOWN",
+                 universe = unique(bg_genes$gene),
+                 gene2Symbol = character(0),
+                 geneSets = bg_genes)
+class(enrichres)
+
+## VISUALISATION ---------------------------------------------------------------
+
+p1 <- barplot(enrichres, showCategory = 20)
+p2 <- mutate(enrichres, qscore = -log(p.adjust, base = 10)) %>% 
+  barplot(x = "qscore")
+p3 <- dotplot(enrichres, showCategory = 15) + ggtitle("Mock vs Infected")
+
+enrichres_eid <- setReadable(enrichres, 'org.Hs.eg.db', 'SYMBOL')
+p4 <- cnetplot(enrichres)
+
+?cnetplot
 
 
+enrichres2 <- pairwise_termsim(enrichres)
 
+treeplot(enrichres2)
 
+emapplot(enrichres2)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+upsetplot(enrichres)
 
 
 
